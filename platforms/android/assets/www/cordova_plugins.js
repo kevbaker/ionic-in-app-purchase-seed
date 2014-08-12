c6c4e2d15de72912a9205ed8c1cd109265dd3c61
Kevin Baker <kbaker@kev.io>
2014-08-12 15:12:33 -0700

    added ios iap plugin

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cc.fovea.plugins.inapppurchase": "3.4.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cc.fovea.plugins.inapppurchase/InAppPurchase.js",
+        "id": "cc.fovea.plugins.inapppurchase.InAppPurchase",
+        "clobbers": [
+            "storekit"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cc.fovea.plugins.inapppurchase": "3.4.1"
+}
+// BOTTOM OF METADATA
+});
