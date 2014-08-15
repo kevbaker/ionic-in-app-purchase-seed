da32ff0b4357c254b3f8997a35ecf86d55e369f2
Kevin Baker <kbaker@kev.io>
2014-08-15 08:38:41 -0700

    working connect to apple for IAP

--- www/js/app.js
+++ www/js/app.js
@@ -50,6 +50,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/products');
+  $urlRouterProvider.otherwise('/app/product');
 });
 
